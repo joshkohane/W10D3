64152a0fe311c3958cc64edfe3fd4d6271d29b27
joshkohane <joshkohane@gmail.com>
2020-11-25 18:00:25 -0500

    Initialize Todos App

---
+++ todos/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+module.exports = {
+    entry: './frontend/components/todo_redux.jsx',
+    output: {
+        path: path.resolve(__dirname),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+};
